3152064405c7384496e89dc3c7ee4a8f2312c1df
unbadfish <3066893506@qq.com>
2021-08-24 09:07:59 +0800

    重构. "优化"了cookies

--- README.md
+++ README.md
@@ -3,58 +3,45 @@
 
 ## 使用方法
 
-+ mainway:输入歌单或专辑的网址等，对应关系如下
+1. 不带参启动，进入循环模式
 
-    ```python
-    in_search_dict = {'^.*?playlist.*?([0-9]+).*$': 'playlist', '^.*?album.*?([0-9]+).*$': 'album', '^list.*?([0-9]+).*$': 'playlist', '^p([0-9]+)$': 'playlist', '^a([0-9]+)$': 'album'}
-      # ...
-    While True:
-          # ...
-        for each_str in list(in_search_dict.keys()):
-            in_match = re.match(each_str, user_in)
-            if in_match:
-                in_kind = in_search_dict.get(each_str)  # get the value
-                in_id = in_match.group(1)  # use re
-    ```
+2. 【py文件路径】 【想要查找的内容】（中间有空格）（已知Bug：网址含有'&'时会报错。<u>不影响正常使用</u>）
 
-+ bylink:输入歌单或专辑的网址，主要的作用是网易云防止哪天偷偷改歌单和专辑的网址……不过这个对输入的要求更加严格：
+3. 【py文件路径】 -h 查看使用方法
 
-    > ‘’’DO NOT use a ".../#/my/m/music/..."link
-    >     Use a link like>>http://music.163.com/playlist?id=123456789
-    >     DO NOT use a link like>>https://music.163.com/#/my/m/music/playlist?id=123456789‘’’
-    >
-    > (代码原注释)
-
-+ byjsonfull:输入“字符串”(见str-get.md)
-
-+ byjsonshort:输入“短字符串”(见str-get.md)
-
-+ byre:输入“字符串”或“短字符串”(见str-get.md)
-
-## 依赖环境
-
-Python(这不是废话吗)
-
-BeautifulSoup4
-
-+ 使用`pip install beautifulsoup4`安装
-
-<u>如有使用.exe可执行文件的需要，欢迎提出issue并附上邮箱地址，exe文件是真的大……</u>
-
-## 原理简述
-
-使用requests获取网页，使用BeautifulSoup解析
-
-当然byjson和byre就没有获取网页的过程了
-
-## 警告 
+## 关于cookies
 
 requests.get并未考虑cookies，根据用户的反馈情况，未登陆的情况下只能获取10首歌。
 
 请前往[这里](https://curl.trillworks.com/)获得cookies
+
+0. 在【https://curl.trillworks.com/】 将网易云网页的curl命令转换为cookies
+
+1. 为避免混乱，cookies文件请尽量使用绝对路径
+
+2. 将得到的cookies中倒数第二行('MUSIC_U': ...)最后的逗号删掉!
+
+3. 将得到的cookies中第一行的【cookies = 】删掉,使文件以‘{’开头，以‘}’结尾
+
+4. 文件示范
+
+    ```json
+    {
+        'JSESSIONID-WYYY': 'asd',
+        # ...
+        'MUSIC_U': 'asd'
+    }
+    ```
 
 ## 友情链接
 
 这个项目是我给[163MusicLyrics](https://github.com/jitwxs/163MusicLyrics)写的配套小程序
 
 链接:https://github.com/jitwxs/163MusicLyrics
+
+## 更新日志
+2021-08-24 重构。"优化"了cookies。老版本请前往old文件夹查看
+
+2020-07-15 增加cookies的获得要求
+
+2020-05-14 首个版本
--- mainway.py
+++ mainway.py
@@ -1,117 +1,237 @@
 # -*- coding: UTF-8 -*-
 
 import requests
-from bs4 import BeautifulSoup as Soup
+from bs4 import BeautifulSoup as bs
 import re
+import json
+import sys
+import pyperclip
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+f_cookies = open(
+    "F:\\Script\\Daily_Script\\Dev\\NetEase-id-geter\\cookies.txt", 'r')
+# 0. 在【https://curl.trillworks.com/】 将网易云网页的curl命令转换为cookies
+# 1. 为避免混乱，请尽量使用绝对路径
+# 2. 将得到的cookies中倒数第二行('MUSIC_U': ...)最后的逗号删掉!
+# 3. 将得到的cookies中第一行的【cookies = 】删掉,使文件以‘{’开头，以‘}’结尾
+"""文件示范:
+{
+    'JSESSIONID-WYYY': 'asd',
+    # ...
+    'MUSIC_U': 'asd'
+}
+"""
+cookies: dict = json.loads(f_cookies.read().replace('\'', '\"'))
+f_cookies.close()
 
-in_search_dict = {'^.*?playlist.*?([0-9]+).*$': 'playlist', '^.*?album.*?([0-9]+).*$': 'album',
-                  '^list.*?([0-9]+).*$': 'playlist', '^p([0-9]+)$': 'playlist',
-                  '^a([0-9]+)$': 'album'}
-# you can add something else in
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Cache-Control': 'max-age=0',
+    'TE': 'Trailers'
+}
 
-print_when_start = '''author:unbadfish@github
+
+def get_album_info(album_id: str) -> str:
+    """
+    用英文逗号(,)合并专辑中每首歌的id
+    :param album_id: 专辑id(str)
+    :return: 专辑中每首歌的id用英文逗号(,)合并后的结果
+    """
+    tittles: list = []
+    song_ids: list = []
+    response = requests.get(
+        f'http://music.163.com/album?id={album_id}', headers=headers, cookies=cookies)
+    response.encoding = 'utf-8'
+    soup = bs(response.text, features='html.parser')
+    # print(soup.prettify())
+    """====debug====
+    f_debug = open(f'album_{album_id}.html', 'w', encoding='utf-8')
+    # f_debug = open(f'album_{album_id}.html', 'w', encoding='utf-8')
+    f_debug.writelines(str(soup.prettify()))
+    # """
+    for each_tittle_div in soup.find_all('div', class_="tit"):
+        # print(each_tittle_div)
+        for each_tittle_tag in each_tittle_div.find_all('h2', class_="f-ff2"):
+            # print(each_tittle_tag)
+            if each_tittle_tag.string.parent == each_tittle_tag and each_tittle_tag.string is not None:
+                # print('===is tittle:===\n', each_tittle_tag, '\n================')
+                tittles.append(str(each_tittle_tag.string))
+    # print(tittles)
+    if len(tittles) == 1:
+        print('标题匹配成功, 请核对:\n【' + tittles[0] + '】')
+    elif len(tittles) > 1:
+        print('匹配到多个标题, 可能是网易云页面布局改变或要查找的内容已被删除，请前往Issue页面,寻找解决方法或提出Issue\n'
+              'Issue页面:http://github.com/unbadfish/NetEase-id-geter/issues')
+        print('将显示所有疑似标题:\n【' + '】【'.join(tittles) + '】')
+    else:
+        print('未匹配到标题, 可能是网易云页面布局改变或要查找的内容已被删除，请前往Issue页面,寻找解决方法或提出Issue\n'
+              'Issue页面:http://github.com/unbadfish/NetEase-id-geter/issues')
+
+    for each_song_div in soup.find_all('div', id="song-list-pre-cache"):
+        # print(each_song_div)
+        for each_song_tag in each_song_div.find_all('a', href=re.compile(r"^/song\?id=\d+$")):
+            # print(each_song_tag)
+            if each_song_tag.string.parent == each_song_tag and each_song_tag.string is not None:
+                song_id_match = re.match(
+                    r"^/song\?id=(\d+)$", str(each_song_tag.get('href')))
+                if song_id_match:
+                    song_ids.append(str(song_id_match.group(1)))
+    # print(song_ids)
+    if len(song_ids) > 0:
+        song_ids_string = ','.join(song_ids)
+        print('歌曲id:\n' + song_ids_string)
+        return song_ids_string
+    else:
+        print('未匹配到歌曲id, 可能是网易云页面布局改变或要查找的内容已被删除，请前往Issue页面,寻找解决方法或提出Issue\n'
+              'Issue页面:http://github.com/unbadfish/NetEase-id-geter/issues')
+        return ''
+
+
+def get_playlist_info(playlist_id: str) -> str:
+    """
+    用英文逗号(,)合并歌单中每首歌的id
+    :param playlist_id: 歌单id(str)
+    :return: 歌单中每首歌的id用英文逗号(,)合并后的结果
+    """
+    tittles: list = []
+    song_ids: list = []
+    response = requests.get(
+        f'http://music.163.com/playlist?id={playlist_id}', headers=headers, cookies=cookies)
+    response.encoding = 'utf-8'
+    soup = bs(response.text, features='html.parser')
+    # print(soup.prettify())
+    """====debug====
+    f_debug = open(f'playlist_{playlist_id}.html', 'w', encoding='utf-8')
+    # f_debug = open(f'playlist_{playlist_id}.html', 'w', encoding='utf-8')
+    f_debug.writelines(str(soup.prettify()))
+    # """
+    for each_tittle_div in soup.find_all('div', class_="tit tit2"):
+        # print(each_tittle_div)
+        for each_tittle_tag in each_tittle_div.find_all('h2', class_="f-ff2 f-brk"):
+            # print(each_tittle_tag)
+            if each_tittle_tag.string.parent == each_tittle_tag and each_tittle_tag.string is not None:
+                # print('===is tittle:===\n', each_tittle_tag, '\n================')
+                tittles.append(str(each_tittle_tag.string))
+    # print(tittles)
+    if len(tittles) == 1:
+        print('标题匹配成功, 请核对:\n【' + tittles[0] + '】')
+    elif len(tittles) > 1:
+        print('匹配到多个标题, 可能是网易云页面布局改变或要查找的内容已被删除，请前往Issue页面,寻找解决方法或提出Issue\n'
+              'Issue页面:http://github.com/unbadfish/NetEase-id-geter/issues')
+        print('将显示所有疑似标题:\n【' + '】【'.join(tittles) + '】')
+    else:
+        print('未匹配到标题, 可能是网易云页面布局改变或要查找的内容已被删除，请前往Issue页面,寻找解决方法或提出Issue\n'
+              'Issue页面:http://github.com/unbadfish/NetEase-id-geter/issues')
+
+    for each_song_div in soup.find_all('div', id="song-list-pre-cache"):
+        # print(each_song_div)
+        for each_song_tag in each_song_div.find_all('a', href=re.compile(r"^/song\?id=\d+$")):
+            # print(each_song_tag)
+            if each_song_tag.string.parent == each_song_tag and each_song_tag.string is not None:
+                song_id_match = re.match(
+                    r"^/song\?id=(\d+)$", str(each_song_tag.get('href')))
+                if song_id_match:
+                    song_ids.append(str(song_id_match.group(1)))
+    # print(song_ids)
+    if len(song_ids) > 0:
+        song_ids_string = ','.join(song_ids)
+        print('歌曲id:\n' + song_ids_string)
+        return song_ids_string
+    else:
+        print('未匹配到歌曲id, 可能是网易云页面布局改变或要查找的内容已被删除，请前往Issue页面,寻找解决方法或提出Issue\n'
+              'Issue页面:http://github.com/unbadfish/NetEase-id-geter/issues')
+        return ''
+
+
+def get_song_id(song_id: str) -> str:
+    """
+    返回单曲id
+    :param song_id: 单曲id
+    :return: 单曲id
+    """
+    # TODO 查询api, 查找歌曲有效性及信息
+    print('您输入的是单曲, id:\n' + song_id)
+    return song_id
+
+
+def get_id(ori_kind: str, ori_id: str) -> str:
+    """
+    主函数
+    :param ori_kind: 类型(str)
+    :param ori_id: id(str)
+    :return: 每首歌的id用英文逗号(,)合并后的结果
+    """
+    if ori_kind == 'album':
+        return get_album_info(ori_id)
+    elif ori_kind == 'playlist':
+        return get_playlist_info(ori_id)
+    elif ori_kind == 'song':
+        return get_song_id(ori_id)
+    else:
+        print('程序执行有误, 请检查')
+        return ''
+
+
+reg_dict: dict = {r'^http[s]?://music\.163\.com/album\?id=(\d+)': 'album',
+                  r'^http[s]?://music\.163\.com/playlist\?id=(\d+)': 'playlist',
+                  r'^http[s]?://music\.163\.com/song\?id=(\d+)': 'song',
+
+                  r'^http[s]?://music\.163\.com/#/album\?id=(\d+)': 'album',
+                  r'^http[s]?://music\.163\.com/#/playlist\?id=(\d+)': 'playlist',
+                  r'^http[s]?://music\.163\.com/#/song\?id=(\d+)': 'song',
+                  r'^a([\d]+)$': 'album', r'^album([\d]+)$': 'album',
+                  r'^p([\d]+)$': 'playlist', r'^playlist([\d]+)$': 'playlist',
+                  r'^song([\d]+)$': 'song'}
+print('''author:unbadfish@github
 Here I declear that this program is written by MYSELF
 Copyright 2020 unbadfish@github
 Licensed under the Apache License, Version 2.0
-'''
+======START======''')
+# *debug
+# print(get_album_info('96276728'))
+# print(get_playlist_info('4866048742'))
+# print(get_id('album', '96276728'))
+# print(get_id('playlist', '4866048742'))
 
-
-def get_id(kind, content):
-    id_work = ''
-    if kind == 'playlist':
-        response = requests.get('https://music.163.com/playlist?id=' + content, headers=headers)
-        response.encoding = 'utf-8'
-        doc = response.text
-        work = Soup(doc, features='html.parser')
-        # ------
-        tittle = work.find_all('h2', class_='f-ff2 f-brk')
-        '''I noticed that only playlists' tittle use <h2 class="f-ff2 f-brk">......</h2>
-        and its parent tag is <div class="tit">(<h2>)...(<h2>)</div>
-        interesting'''
-        if str(tittle) != '[]':
-            out_tittle = tittle[0].string  # <<<<<<------
-        else:
-            out_tittle = ''
-        all_id = work.find_all('a', href=re.compile("/song\?id=[0-9]+"))
-        '''all pages use <a href="/song?id=123456">...</a> to mark songs'''
-        for one in all_id:
-            link = one.get('href')
-            match2 = re.match('^.*?/song\?id=([0-9]+).*?$', link)
-            one_id = match2.group(1)
-            id_work = id_work + one_id + ','
-        out_id = id_work[0:-1]  # <<<<<<------
-        if out_tittle != '' and out_id != '':
-            print(out_tittle)
-            print(out_id)  # like>>1420663042,1423985980
-        else:
-            print('404 NOT FOUND')
-    elif kind == 'album':
-        response = requests.get('http://music.163.com/album?id=' + content, headers=headers)
-        response.encoding = 'utf-8'
-        doc = response.text
-        work = Soup(doc, features='html.parser')
-        # ------
-        tittle = work.find_all('h2', class_='f-ff2')
-        '''I noticed that only albums' tittle use <h2 class="f-ff2">...</h2>
-        and its parent tag is <div class="tit">(<h2>)...(<h2>)</div>
-        interesting'''
-        if str(tittle) != '[]':
-            out_tittle = tittle[0].string  # <<<<<<------
-        else:
-            out_tittle = ''
-        all_id = work.find_all('a', href=re.compile("/song\?id=[0-9]+"))
-        '''all pages use <a href="/song?id=123456">...</a> to mark songs'''
-        for one in all_id:
-            link = one.get('href')
-            match3 = re.match('^.*?/song\?id=([0-9]+).*?$', link)
-            one_id = match3.group(1)
-            id_work = id_work + one_id + ','
-        out_id = id_work[0:-1]  # <<<<<<------
-        if out_tittle != '' and out_id != '':
-            print(out_tittle)
-            print(out_id)  # like>>1420663042,1423985980
-        else:
-            print('404 NOT FOUND')
-    else:
-        print('Wrong kind.Try again')
-    '''--debug--
-    file = open(kind + content + '.html', mode='w', encoding='utf-8')
-    # file.writelines(str(work.prettify()))  # can NOT be read by BeautifulSoup,but you can read it more clearly
-    file.writelines(str(work))  # can be read by BeautifulSoup
-    # both files can NOT be read by explorer.Test in FireFox
-    file.flush()
-    file.close()
-    # '''
-
-
-print(print_when_start)
-'''--debug--
-get_id('playlist', '123456789')
-get_id('album', '66666')
-# the in_kind and in_id must be 'str' kind'''
-while True:
-    i = int(0)
-    in_kind = None
-    in_id = None
-    print('Paste the share link')
-    user_in = input()
-    if user_in.casefold() == 'bye':
-        break
-    print('Checking info...')
-    for each_str in list(in_search_dict.keys()):
-        in_match = re.match(each_str, user_in)
-        if in_match:
-            in_kind = in_search_dict.get(each_str)  # get the value
-            in_id = in_match.group(1)  # use re
-            print(in_kind.capitalize() + 'id:' + in_id)
+if len(sys.argv) == 1:
+    # *Cycle mode
+    while True:
+        match_state: bool = False
+        print('输入想要查找的内容')
+        user_input: str = input()
+        if user_input.casefold() == 'bye':
             break
-        else:
-            i = i + 1
-            # print(i)
-    if i == len(in_search_dict) and in_kind is None and in_id is None:
-        print('Wrong input.Check again')
-    else:
-        get_id(in_kind, in_id)
-input('press ENTER key to exit')
+        for each_reg in reg_dict.keys():
+            match_input = re.match(each_reg, user_input)
+            # print(match_input)
+            if match_input:
+                match_state: bool = True
+                input_id = match_input.group(1)
+                input_kind = reg_dict.get(each_reg)
+                # print(input_kind, input_id)
+                print(f'输入匹配成功, 将查询 {input_kind} {input_id} 的信息')
+                pyperclip.copy(get_id(input_kind, input_id))
+        if match_state is False:
+            print('输入有误, 请检查后重输')
+    input('press ENTER key to exit')
+elif len(sys.argv) == 2 and sys.argv[1] != '-h':
+    # *Arg mode
+    match_state: bool = False
+    for each_reg in reg_dict.keys():
+        match_input = re.match(each_reg, sys.argv[1])
+        # print(match_input)
+        if match_input:
+            match_state: bool = True
+            input_id = match_input.group(1)
+            input_kind = reg_dict.get(each_reg)
+            # print(input_kind, input_id)
+            print(f'输入匹配成功, 将查询 {input_kind} {input_id} 的信息')
+            get_id(input_kind, input_id)
+    if match_state is False:
+        print('输入有误, 请检查后重输')
+else:
+    print('获取网易云id\n用法: \n1. 不带参启动，进入循环模式\n'
+          '2. %s 【想要查找的内容】（已知Bug：网址含有\'&\'时会报错。不影响正常使用）' % sys.argv[0])
